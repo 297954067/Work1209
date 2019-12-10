381c56b7e535318a4a9e2b7af3b5a19645e0811d
王奕坤 <wykwykwyk1234@qq.com>
2019-12-10 14:56:16 +0800

    王：role添加了tostring方法

--- src/main/java/com/bean/Role.java
+++ src/main/java/com/bean/Role.java
@@ -3,5 +3,9 @@ package com.bean;
 public class Role {
 	private Integer role_id;
 	private String role_name;
+	@Override
+	public String toString() {
+		return "Role [role_id=" + role_id + ", role_name=" + role_name + "]";
+	}
 
 }
