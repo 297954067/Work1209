8073a1b3f0f042c75035e9430f72246e19c74691
王奕坤 <wykwykwyk1234@qq.com>
2019-12-10 14:29:42 +0800

    提交了最初项目

---
+++ src/main/java/com/bean/Users.java
@@ -0,0 +1,42 @@
+package com.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Users {
+private Integer user_Id;
+private String user_name;
+private String user_pwd;
+private String user_role;
+private String user_status;
+public Integer getUser_Id() {
+	return user_Id;
+}
+public void setUser_Id(Integer user_Id) {
+	this.user_Id = user_Id;
+}
+public String getUser_name() {
+	return user_name;
+}
+public void setUser_name(String user_name) {
+	this.user_name = user_name;
+}
+public String getUser_pwd() {
+	return user_pwd;
+}
+public void setUser_pwd(String user_pwd) {
+	this.user_pwd = user_pwd;
+}
+public String getUser_role() {
+	return user_role;
+}
+public void setUser_role(String user_role) {
+	this.user_role = user_role;
+}
+public String getUser_status() {
+	return user_status;
+}
+public void setUser_status(String user_status) {
+	this.user_status = user_status;
+}
+}
